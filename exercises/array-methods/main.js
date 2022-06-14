f6dbf7555d195bf0712f09332f135e353141a8f8
Evanperez23 <evanperez23@gmail.com>
2022-06-14 10:58:12 -0500

    update

--- exercises/array-methods/main.js
+++ exercises/array-methods/main.js
@@ -2,3 +2,5 @@ const readline = require("readline-sync")
 
 var fruit = ["banana", "apple", "orange", "watermelon"];
 var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+var newArr = fruit.concat(vegetables)
+console.log(newArr)
---
+++ exercises/forception/main.js
@@ -0,0 +1,18 @@
+const readline = require("readline-sync")
+
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var updatedArr = [];
+function forception(){
+    for(var i = 0; i < people.length; i++){
+        updatedArr.push(people[i] + ":")
+        for(var j = 0; j < alphabet.length; j++){
+            updatedArr.push(alphabet[j].toUpperCase().split())
+        }
+        console.log(updatedArr);
+    }
+    return updatedArr;
+
+}
+forception();
